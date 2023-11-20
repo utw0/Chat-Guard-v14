73a3348de47c5a6c55c986e008c2c843c3afa53f
luhux <74924310+utw0@users.noreply.github.com>
2023-11-21 01:11:27 +0300

    Update GuildSchema.js

--- Global/Settings/Models/GuildSchema.js
+++ Global/Settings/Models/GuildSchema.js
@@ -14,21 +14,4 @@ module.exports = vanteDB.model({
     Folder: './Global/Database/',
     Cluster: true,
     Type: [],
-}, guildSchema); //mongo yok değilmi bunda
-// true yaptık açtık işte // bu adece dosya yukelemelri yapiyor find update gibi seylerde options da cluster belirtmen lazim
-// göstersene bi
-
-// vantedb.findOne({guildID: ertununAnnesi}, { Cluster: "ServerID" }) // butun olaylarda en sonda options kismi oluyor onun icine Cluster belirtirsen oluyor
-// tamamen local benim db uzdrinden calisiyor ama cluster modu aktif degil cluster acarsaniz sunucuya ozel dosya acar icine jsonlari acar
-// anladım şimdi
-
-// altyapida baska anlamadiign bir sey varsa aciklayim gidicem
-//anlamadığım bişey olduğunda dmden spam atarım merak etme canım <3
-// ananskm ama ilk ertunn anensini sonr zaten yorulur sikmem anneni merak etme
-// çok teşekkürler <3 
-// rica ederimsaas
-
-
-// evet cluster modu acik olanlarda yapman lazim diger turlu bulamiyor dosyayi tamam canım ertunun annesi için kolay gelsin beline kuvvet iyi çalışamlar dilerim link bekliyorum
-// eyvallah hocam ama paylasmam ozelimi karim o benm :haha:
-// buton gibi interaction tabanli seylerde client.on interactionCreatee acmayin events dosyasinda custom var orda hem menu hem button icin handler var tamam data silince de cluster eklicez değil mi veya update edince
+}, guildSchema); 
